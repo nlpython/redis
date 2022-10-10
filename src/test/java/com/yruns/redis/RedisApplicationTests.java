0d5627e6e36b9c18373029c4922f4093ab10fde2
ysh <2994056734@qq.com>
2022-10-10 23:21:20 +0800

    udpate

---
+++ src/test/java/com/yruns/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yruns.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
